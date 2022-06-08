3000214ef0bdbf08b1ed974af30f28988fdd5e34
Ben Yeager <yeager.ben.a@gmail.com>
2022-06-08 18:53:14 -0400

    further automated the download of Google Maps review data in preparation for full automation of all data download

--- model/download_maps_reviews.py
+++ model/download_maps_reviews.py
@@ -1,15 +1,37 @@
 '''
-This code pulls Google Maps review data using the Google Maps API
-and saves to file
-'''
+This code searches Google Maps for restaurants in NYC DOHMH dataset and
+downloads matching restaurant details and reviews.
 
+The script is called from the command line via
+
+    python3 download_maps_reviews.py <nreviews>
+
+where <nreviews> is the number of reviews to download. ***Note
+that the Google Maps API is not 100% free. Take care not download more than
+you want to pay for beyond the monthly free allowance!***
+
+In order to run this code, you will need a working Google Maps API key stored
+in ../API_keys/API_key_GoogleMaps_build.txt.
+
+This code also requires the NY DOHMH dataset to be downloaded first and stored
+in ./data/raw_data/DOHMH_New_York_City_Restaurant_Inspection_Results.csv.
+'''
 import googlemaps
 import pandas as pd
-import requests
-from pprint import pprint
+import argparse
+# import requests
 import dill
 
-# datafile = './data/GMaps_data_1_10_test.pkd'
+# -----------------------------------------------------------------------------
+# PARSE INPUT
+# -----------------------------------------------------------------------------
+parser = argparse.ArgumentParser(
+    prog = 'ProgramName',
+    description = 'What the program does',
+    epilog = 'Text at the bottom of help',
+)
+parser.add_argument('num_establishments', type=int)
+args = parser.parse_args()
 
 # -----------------------------------------------------------------------------
 # OPEN GMAPS API SESSION
@@ -27,33 +49,52 @@ gmaps = googlemaps.Client(key = APIkey)
 df = pd.read_csv('data/raw_data/DOHMH_New_York_City_Restaurant_Inspection_Results.csv',
                     # index_col = 'CAMIS',
                     usecols = ['CAMIS', 'DBA', 'BORO', 'CUISINE DESCRIPTION',
-                                # 'INSPECTION DATE',
-                                # 'VIOLATION CODE',
-                                # 'VIOLATION DESCRIPTION', 'CRITICAL FLAG',
-                                # 'SCORE',
-                                # 'GRADE',
                                 'Latitude', 'Longitude']
 )
 
-by_rest = df.groupby([
+# Sort restaurants by length of inspection history - let's get the restaurants
+# with the most data first
+rest_df = df.groupby([
     'CAMIS', 'DBA', 'BORO', 'CUISINE DESCRIPTION',
     'Latitude', 'Longitude'],
     as_index=False).size()
-by_rest = by_rest.sort_values('size', ascending=False, ignore_index=True)
+rest_df = rest_df.sort_values('size', ascending=False, ignore_index=True)
 
-print(f'Total restaurants: {len(by_rest)}')
+print(f'\nTotal inspected establishments: {len(rest_df)}')
+
+# -----------------------------------------------------------------------------
+# Get list of restaurants to search for
+# -----------------------------------------------------------------------------
+# CAMIS IDs in existing Maps data - don't re-search these
+import glob
+maps_data_files = sorted(glob.glob('data/raw_data/gmaps_restaurant_data_*.pkd'))
+known_CAMIS = []
+for file in maps_data_files:
+    f = open(file, 'rb')
+    maps_data = dill.load(f)
+    known_CAMIS.extend(pd.json_normalize(maps_data)['CAMIS'].to_list())
+nknown = len(known_CAMIS)
+print(f'Previously downloaded Maps establishments: {nknown}')
+
+# Limit to establishments whose data we don't already have
+rest_df = rest_df[~rest_df['CAMIS'].isin(known_CAMIS)]
+# Limit number of establishments to designated number
+rest_df = rest_df.iloc[:args.num_establishments]
 
 # -----------------------------------------------------------------------------
 # Loop through restaurants and retrieve details from Google Maps
 # -----------------------------------------------------------------------------
-datalist = []; old_j = 0
-for j in range(18000,20000):
+print(f'Searching for {args.num_establishments} additional establishments\n')
+datalist = []; #old_j = 0
+for j, row in rest_df.iterrows():
+    # print(row)
+    # print('\n')
 
     # Restaurant details from df
-    rcamis = by_rest.loc[j, 'CAMIS']
-    rname = by_rest.loc[j, 'DBA']
-    rlat = by_rest.loc[j, 'Latitude']
-    rlon = by_rest.loc[j, 'Longitude']
+    rcamis = row['CAMIS']
+    rname = row['DBA']
+    rlat = row['Latitude']
+    rlon = row['Longitude']
     loc_string = 'point:'+str(rlat)+','+str(rlon)
 
     # Search restaurant on Maps using name and lat/lon
@@ -91,13 +132,9 @@ for j in range(18000,20000):
     datalist.append(restdata)
 
     # Status update
-    if (j % 100) == 0:
-        print(f'Searched {j}/{len(by_rest)} restaurants.')
+    # if ((j-nknown) / args.num_establishments)*100 % 5 == 0:
+    print(f'Progress: {((j-nknown+1)/args.num_establishments)*100} %', end='\r')
 
     # Dump to file
-    if (j+1) % 1000 == 0:
-        print(f'Dumping entries {old_j}-{j} to file.')
-        dill.dump(datalist, open(f'./data/raw_data/gmaps_restaurant_data_{j:05d}.pkd', 'wb'))
-
-        old_j = j
-        datalist = []
+    fname = f'./data/raw_data/gmaps_restaurant_data_{len(known_CAMIS)}-{len(known_CAMIS)+args.num_establishments-1}.pkd'
+    dill.dump(datalist, open(fname, 'wb'))
